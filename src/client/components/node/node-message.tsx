cd9ed8f2656d76ed12122dc46232a10a0c9227c8
Xavier Brown <secton67@gmail.com>
2018-04-20 09:41:22 -0500

    only show message name/timestamp if distinct

--- src/client/components/node/node-message.tsx
+++ src/client/components/node/node-message.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as moment from "moment";
+import * as classnames from "classnames";
 
 import styled, { keyframes } from "styled-components";
 
@@ -8,6 +9,7 @@ import { MessageType } from "../../models/message.model";
 const anim_slide = require("react-animations").slideInUp;
 const AnimatedLi = styled.li`
     animation: 0.4s ${keyframes`${anim_slide}`};
+    display: flex;
 `;
 
 export interface NodeMessageProps {
@@ -16,39 +18,25 @@ export interface NodeMessageProps {
     name: string | null;
     content: string;
     sent_at: string;
-}
-
-const renderMessage = (type: MessageType, name: string | null, content: string, sent_at?: string) => {
-    const timestamp = <span className="node-message-timestamp">{moment(sent_at).format("h:mm A")}</span>;
-    const display_name = name ? <strong className="node-message-author">{name}</strong> : null;
-    const cont = <span className="node-message-content">{content}</span>;
-    switch(type) {
-        case MessageType.SYSTEM:
-            return (
-                <AnimatedLi className="node-message node-message-system">
-                    {timestamp}{cont}
-                </AnimatedLi>
-            );
-        case MessageType.CHAT:
-            return (
-                <AnimatedLi className="node-message node-message-chat">
-                    {timestamp}{display_name}{cont}
-                </AnimatedLi>
-            );
-        case MessageType.ACTION:
-            return (
-                <AnimatedLi className="node-message node-message-action">
-                    {timestamp}{display_name}{cont}
-                </AnimatedLi>
-            );
-        default:
-            return null;
-    }
+    show_meta: boolean;
 }
 
 export class NodeMessage extends React.Component<NodeMessageProps, {}> {
     render() {
-        const { name, content, type, sent_at } = this.props;
-        return renderMessage(type, name, content, sent_at);
+        const { name, content, type, sent_at, show_meta } = this.props;
+        const timestamp = <span className={ classnames("node-message-timestamp", { hidden: !show_meta }) }>{moment(sent_at).format("h:mm A")}</span>;
+        const display_name = name ? <span className={ classnames("node-message-author", { hidden: !show_meta }) }>{name}</span> : null;
+        const cont = <span className="node-message-content">{content}</span>;
+        const message_class = classnames("node-message", {
+            "node-message-system": type === MessageType.SYSTEM,
+            "node-message-chat": type === MessageType.CHAT,
+            "node-message-action": type === MessageType.ACTION
+        });
+        
+        return (
+            <AnimatedLi className={message_class}>
+                {timestamp}{display_name}{cont}
+            </AnimatedLi>
+        );
     }
 }
